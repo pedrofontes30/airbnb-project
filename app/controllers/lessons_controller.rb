5c6e2867ad81d4ed2634a380be00891ff5967ada
Pedro Fontes <pedrosafontes.psf@gmail.com>
2020-06-23 10:36:55 +0100

    Show page basic layout

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,2 +1,8 @@
 class LessonsController < ApplicationController
+  def show
+    @lesson = Lesson.find(params[:id])
+    @user = User.find(1)
+    @appointment = Appointment.new
+    @review = Review.new
+  end
 end
