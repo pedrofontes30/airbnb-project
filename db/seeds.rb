0ae7c312a8a11f7f94ec8f52a50a59647f653813
nunolei <nunomartinslei@gmail.com>
2020-06-24 10:11:22 +0100

    conflits in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-<<<<<<< HEAD
 Review.destroy_all
 Lesson.destroy_all
 Appointment.destroy_all
@@ -29,29 +28,3 @@ lesson = Lesson.create( description: "Lesson for any beginners who want to have 
 Appointment.create(user: user2, lesson: lesson)
 
 Review.create(content: 'Lots of fun in my first day of surfing!', rating: 5, lesson_id: lesson.id, user: user2)
-
-  # create_table "users", force: :cascade do |t|
-  #   t.string "email", default: "", null: false
-  #   t.string "encrypted_password", default: "", null: false
-  #   t.string "reset_password_token"
-  #   t.datetime "reset_password_sent_at"
-  #   t.datetime "remember_created_at"
-  #   t.datetime "created_at", precision: 6, null: false
-  #   t.datetime "updated_at", precision: 6, null: false
-  #   t.string "first_name"
-  #   t.string "last_name"
-  #   t.integer "age"
-  #   t.string "role"
-  #   t.index ["email"], name: "index_users_on_email", unique: true
-  #   t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  # end
-=======
-
-User.create(email: "user@gmail.com", password: "123456")
-
-10.times do
-Lesson.create(description: "Lesson for any beginners who want to have fun and learn the basics of surfing!", start_time: "9", end_time: "11", week_day: "saturday", location: "Caparica", user_id: User.first.id, max_attendees: 5)
-end
-
-
->>>>>>> master
