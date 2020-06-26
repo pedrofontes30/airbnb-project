45e075863cad8271b277de5c5a62ff0c40640731
cmscoder <camila.medeiros.silva2013@gmail.com>
2020-06-26 15:48:06 +0100

    Add the sunshine on the mark map

--- db/schema.rb
+++ db/schema.rb
@@ -56,10 +56,10 @@ ActiveRecord::Schema.define(version: 2020_06_26_104756) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price"
-    t.bigint "sport_id", null: false
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.bigint "sport_id", null: false
     t.string "difficulty"
     t.boolean "equipment"
     t.index ["sport_id"], name: "index_lessons_on_sport_id"
