5d57b0d8db30d2f364cfefa1e4c01da7ea2da871
cmscoder <camila.medeiros.silva2013@gmail.com>
2020-06-26 13:20:38 +0100

    Add the path to show page when we click on popup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,8 +30,11 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../plugins/init_mapbox';
 
+
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
+
 });
+
